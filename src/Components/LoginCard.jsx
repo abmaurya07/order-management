ab637d8c34bd2ffc1e9455cd40eb370b8df1041e
Abhishek <abmaurya07@gmail.com>
2022-08-13 11:30:23 +0530

    Login & Registration done with local storage

--- src/Components/LoginCard.jsx
+++ src/Components/LoginCard.jsx
@@ -1,53 +1,138 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
- const LoginCard = () => {
+const LoginCard = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] =useState ()
+  const [password, setPassword] =useState ()
+  const [userData, setUserData] = useState()
+  const [errorMsg, setErrorMsg] = useState()
+  console.log("userData", userData)
+  useEffect( () => {
+    if(localStorage.getItem('Users')){
+      setUserData(JSON.parse( localStorage.getItem('Users')))
+    }
+  },[])
+
+  function checkValidation() {
+    console.log("email", email, 'password', password)
+    let status = false;
+    let regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    
+    let test = userData.some((e) => e.email != email)
+    console.log("userfld", test)
+    if (email === "" || regex.test(email) === false)
+       {
+        setErrorMsg("Please Enter Valid Email!")
+        return status;
+       }else if (password === "") {
+        setErrorMsg("Please Enter Password")
+        return status;
+       }else if (!userData){
+        setErrorMsg("User not found. Please register.")
+        return status;
+       }else if(userData.some((e) => e.email == email) == false){
+        setErrorMsg("Email or Password is wrong!")
+        return status;
+       }
+       else if (userData.some((e) => e.password == password) ==false) {
+        setErrorMsg("Email or Password is wrong!")
+        return status;
+       }
+       else {
+         setErrorMsg()
+        status = true
+        return status}
+    }
   
-    return (
-        <section class="vh-100 gradient-custom">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-dark text-white" style="border-radius: 1rem;">
-          <div class="card-body p-5 text-center">
+    const handleSubmit = () => {
+      let UserDetails = userData.filter((e) => e.email === email)
+      console.log("userDetail", UserDetails)
+      localStorage.setItem("isLoggedIn", true)
+      localStorage.setItem("userData", JSON.stringify(UserDetails))
+      navigate("/home")
+    }
 
-            <div class="mb-md-5 mt-md-4 pb-5">
+  return (
+    <section className="vh-100 gradient-custom">
+      <div className="container  h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div
+              className="card bg-dark text-white"
+              style={{ borderRadius: "1rem" }}
+            >
+              <div className="card-body p-5 text-center">
+                <div className="mb-md-5 mt-md-4 pb-3">
+                  <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                  <p className="text-white-50 mb-3">
+                    Please enter your email and password!
+                  </p>
 
-              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-              <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                  <div className="form-outline form-white mb-2">
+                    <label className="form-label" for="email">
+                      Email
+                    </label>
 
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                <label class="form-label" for="typeEmailX">Email</label>
+                    <input
+                      type="email"
+                      id="email"
+                      className="form-control form-control-lg"
+                      onChange={(e) => {setEmail(e.target.value)}}
+                    />
+                  </div>
+
+                  <div className="form-outline form-white mb-2">
+                    <label
+                      className="form-label text-right"
+                      for="password"
+                     
+                    >
+                      Password
+                    </label>
+
+                    <input
+                      type="password"
+                      id="password"
+                      className="form-control form-control-lg"
+                      onChange={(e) => {setPassword(e.target.value)}}
+                    />
+                  </div>
+
+                  <p className="small mb-5 pb-lg-2">
+                    <a className="text-white-50" href="#!">
+                      Forgot password?
+                    </a>
+                  </p>
+                  <p className="text-danger">{errorMsg}</p>
+                  <button
+                    className="btn btn-outline-light btn-lg px-5"
+                    type="submit"
+                    onClick={() => {checkValidation()=== true ? handleSubmit() : console.log("invalid Data")}}
+                  >
+                    Login
+                  </button>
+                </div>
+
+                <div>
+                  <p className="mb-0">
+                    Don't have an account?{" "}
+                    <a
+                      href="#!"
+                      className="text-white-50 fw-bold"
+                      onClick={() => navigate("/register")}
+                    >
+                      Sign Up
+                    </a>
+                  </p>
+                </div>
               </div>
-
-              <div class="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                <label class="form-label" for="typePasswordX">Password</label>
-              </div>
-
-              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-
-              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
-              <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-              </div>
-
             </div>
-
-            <div>
-              <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
-              </p>
-            </div>
-
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</section>
-    )
- }
- export default LoginCard;
+    </section>
+  );
+};
+export default LoginCard;
---
+++ src/Components/Register.jsx
@@ -0,0 +1,173 @@
+import React, { useState } from "react";
+import {useNavigate} from "react-router-dom"
+
+const RegisterCard = () => {
+    const [fullName, setFullName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPass, setConfirmPass] = useState("")
+    const [errorMsg, setErrorMsg] = useState("")
+
+
+    const navigate = useNavigate()
+
+    function checkValidation() {
+        let status = false;
+        let regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+         if(fullName === "") {
+          
+                setErrorMsg("Please Enter Full Name!")
+                return status;
+         } else if (email === "" || regex.test(email) === false)
+           {
+            setErrorMsg("Please Enter Valid Email!")
+            return status;
+           }else if (password === "") {
+            setErrorMsg("Please Enter Password")
+            return status;
+           }else if (confirmPass === ""){
+            setErrorMsg("Please Enter the confirm password!")
+            return status;
+           }else if(password !== confirmPass){
+            setErrorMsg("Passowrd & Confirm password does not match!")
+            return status;
+           }
+           else if (password.length < 8) {
+            setErrorMsg("Password Length is too short! Minimum character - 8")
+            return status;
+           }
+           else {
+             setErrorMsg()
+            status = true
+            return status}
+        }
+      
+
+
+const handleSubmit =() => {
+console.log(JSON.parse(localStorage.getItem('Users')))
+
+ let users
+    if(localStorage.getItem('Users') == undefined) {
+        users = []
+    } else {
+ users = JSON.parse( localStorage.getItem('Users'))
+ console.log("opip", users)
+    }
+    let payload = {
+        fullName: fullName,
+        email: email,
+        password: password,
+    }
+
+    users.push(payload)
+
+   let tet = payload
+
+localStorage.setItem('Users', JSON.stringify(users));
+
+}
+
+  return (
+    <section className="vh-100 gradient-custom">
+      <div className="container  h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div
+              className="card bg-dark text-white"
+              style={{ borderRadius: "1rem" }}
+            >
+              <div className="card-body p-5 ">
+                <div className="mb-md-5 mt-md-4 pb-3">
+                  <h2 className="fw-bold pb-2 text-center">SIGN UP</h2>
+                  <div className="form-outline form-white mb-2">
+                    <label className="form-label" for="fullName">
+                      Full Name
+                    </label>
+
+                    <input
+                      type="text"
+                      id="fullName"
+                      className="form-control form-control-lg"
+                      onChange={(e) => {setFullName(e.target.value)}}
+                    />
+                  </div>{" "}
+                  <div className="form-outline form-white mb-2">
+                    <label className="form-label" for="email">
+                      Email Address
+                    </label>
+
+                    <input
+                      type="email"
+                      id="email"
+                      className="form-control form-control-lg"
+                      onChange={(e) => {setEmail(e.target.value)}}
+
+                    />
+                  </div>{" "}
+                  <div className="form-outline form-white mb-2">
+                    <label
+                      className="form-label"
+                      for="password"
+                    >
+                      Password
+                    </label>
+
+                    <input
+                      type="password"
+                      id="password"
+                      className="form-control form-control-lg"
+                      onChange={(e) => {setPassword(e.target.value)}}
+
+                    />
+                  </div>
+                  <div className="form-outline form-white mb-2">
+                    <label
+                      className="form-label text-right"
+                      for="confirmPass"
+                    >
+                      Confirm Password
+                    </label>
+
+                    <input
+                      type="password"
+                      id="confirmPass"
+                      className="form-control form-control-lg"
+                      onChange={(e) => {setConfirmPass(e.target.value)}}
+
+                    />
+                  </div>
+                  <p className="text-danger">{errorMsg}</p>
+               <div className="text-center">
+                  <button
+                    className="btn btn-outline-light btn-lg px-5 mt-3"
+                    type="submit"
+                    onClick={() => {checkValidation() === true ? handleSubmit() : console.log("Invalid Data")}}
+                  >
+                    Sign Up
+                  </button>
+
+                  </div>
+                </div>
+
+                <div>
+                  <p className="mb-0">
+                    Already have and account?{" "}
+                    <a
+                      href="#!"
+                      className="text-white-50 fw-bold"
+                      onClick={() => navigate("/login")}
+                    >
+                      Login
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default RegisterCard;
